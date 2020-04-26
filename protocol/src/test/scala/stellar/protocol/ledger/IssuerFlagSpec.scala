7b161fbfde46d427e05f54517bc6188070fbb642
Jem Mawson <jmawson@squareup.com>
2020-04-26 21:55:47 +1000

    Add IssuerFlags

---
+++ protocol/src/test/scala/stellar/protocol/ledger/IssuerFlagSpec.scala
@@ -0,0 +1,25 @@
+package stellar.protocol.ledger
+
+import org.scalacheck.{Arbitrary, Gen}
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+import stellar.protocol.xdr.Encode
+
+class IssuerFlagSpec extends Specification with ScalaCheck {
+  import IssuerFlags._
+
+  "issuer flag sets" should {
+    "be deserialisable" >> prop { flags: Set[IssuerFlag] =>
+      val encoded = Encode.int(flags.map(_.i + 0).fold(0)(_ | _))
+      val (remaining, value) = IssuerFlagSet.decode.run(encoded).value
+      remaining must beEmpty
+      value mustEqual flags
+    }
+  }
+}
+
+object IssuerFlags {
+  val genIssuerFlags: Gen[Set[IssuerFlag]] = Gen.containerOf[Set, IssuerFlag](
+    Gen.oneOf(AuthorizationRequiredFlag, AuthorizationImmutableFlag, AuthorizationImmutableFlag))
+  implicit val arbIssuerFlags: Arbitrary[Set[IssuerFlag]] = Arbitrary(genIssuerFlags)
+}
