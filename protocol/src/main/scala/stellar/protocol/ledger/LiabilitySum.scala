67495a4ada7599c757268e4acbb7540bd5b0b6a5
Jem Mawson <jmawson@squareup.com>
2020-04-26 17:04:38 +1000

    Add LiabilitySum

---
+++ protocol/src/main/scala/stellar/protocol/ledger/LiabilitySum.scala
@@ -0,0 +1,18 @@
+package stellar.protocol.ledger
+
+import cats.data.State
+import stellar.protocol.xdr.Encode.{int, long}
+import stellar.protocol.xdr.{Decoder, Encodable}
+
+case class LiabilitySum(buying: Long, selling: Long) extends Encodable {
+  override def encode: LazyList[Byte] = long(buying) ++ long(selling) ++ int(0)
+}
+
+object LiabilitySum extends Decoder[LiabilitySum] {
+  val decode: State[Seq[Byte], LiabilitySum] = for {
+    buying <- long
+    selling <- long
+    _ <- int
+  } yield LiabilitySum(buying, selling)
+}
+
