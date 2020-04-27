0d0a74a724d0f519c5af6a01ab19868ac39455fc
Jem Mawson <jmawson@squareup.com>
2020-04-27 21:05:51 +1000

    Add Price

---
+++ protocol/src/main/scala/stellar/protocol/Price.scala
@@ -0,0 +1,16 @@
+package stellar.protocol
+
+import cats.data.State
+import stellar.protocol.xdr.Encode.int
+import stellar.protocol.xdr.{Decoder, Encodable, Encode}
+
+case class Price(n: Int, d: Int) extends Encodable {
+  def encode: LazyList[Byte] = int(n) ++ int(d)
+}
+
+object Price extends Decoder[Price] {
+  val decode: State[Seq[Byte], Price] = for {
+    n <- int
+    d <- int
+  } yield Price(n, d)
+}
---
+++ protocol/src/test/scala/stellar/protocol/PriceSpec.scala
@@ -0,0 +1,23 @@
+package stellar.protocol
+
+import org.scalacheck.{Arbitrary, Gen}
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+
+class PriceSpec extends Specification with ScalaCheck with XdrSerdeMatchers {
+  import Prices._
+
+  "price" should {
+    "serialise and deserialise" >> prop { price: Price =>
+      price must xdrDecodeAndEncode(Price)
+    }
+  }
+}
+
+object Prices {
+  val genPrice: Gen[Price] = for {
+    n <- Gen.posNum[Int]
+    d <- Gen.posNum[Int]
+  } yield Price(n, d)
+  implicit val arbPrice: Arbitrary[Price] = Arbitrary(genPrice)
+}
